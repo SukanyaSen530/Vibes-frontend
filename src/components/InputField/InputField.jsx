a94fa6bab76063dd3c917b1421baf95f55eeb0a1
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-05-13 22:53:24 +0530

    feat - added custom input-field component

---
+++ src/components/InputField/InputField.jsx
@@ -0,0 +1,66 @@
+import "./input-field.scss";
+
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+function InputField({
+  labelName,
+  type,
+  value,
+  name,
+  placeholder,
+  errorMessage,
+  ...other
+}) {
+  const [showPass, setShowPass] = useState(false);
+
+  let content = null;
+
+  if (type === "textarea") {
+    content = (
+      <textarea
+        {...other}
+        placeholder={labelName}
+        className="input-group-new__input w-full p-4"
+        name={name}
+        value={value}
+      />
+    );
+  } else {
+    content = (
+      <input
+        {...other}
+        type={type === "password" ? (showPass ? "text" : "password") : type}
+        className="input-group-new__input text-black p-4 w-full text-justify"
+        placeholder={placeholder ? placeholder : labelName}
+        value={value}
+        name={name}
+      />
+    );
+  }
+
+  return (
+    <div className="input-group-new w-full mt-4 mx-auto mb-8 block relative capitalize">
+      <label
+        className="input-group-new__label text-gray-600 mx-4 mb-4"
+        htmlFor={name}
+      >
+        {labelName}
+      </label>
+      {content}
+
+      <span className="input-group-new__focus-border absolute"></span>
+
+      {type === "password" ? (
+        <span
+          onClick={() => setShowPass((val) => !val)}
+          className="input-group-new__show-pass absolute"
+        >
+          {showPass ? <AiFillEye /> : <AiFillEyeInvisible />}
+        </span>
+      ) : null}
+    </div>
+  );
+}
+
+export default InputField;
