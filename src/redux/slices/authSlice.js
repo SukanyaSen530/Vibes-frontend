db213332caae8651baadf5a75b46eef2bec5185f
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-05-15 11:17:05 +0530

    feat - added authSlice

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -9,17 +9,16 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    authenticateUser: (state) => {
-      state.value += 1;
+    authenticateUser: (state, { payload }) => {
+      state.user = payload.user;
+      state.token = payload.token;
     },
-    logoutUser: (state) => {
-      state.value -= 1;
-    },
+    logoutUser: () => initialState,
   },
 });
 
 export const { authenticateUser, logoutUser } = authSlice.actions;
 
-export const selectCount = (state) => state.counter.value;
+export const selectAuth = (state) => state.auth;
 
 export default authSlice.reducer;
