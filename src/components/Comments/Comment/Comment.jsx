7c5c7e7f6e81d44489f3500cac87a7d8ec2d6fb1
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-05-24 14:12:14 +0530

    feat - added like/dislike comment feature

--- src/components/Comments/Comment/Comment.jsx
+++ src/components/Comments/Comment/Comment.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 import CommentBody from "./CommentBody";
 import CommentFooter from "./CommentFooter";
 
 const Comment = ({ comment }) => {
-  const { user, content, createdAt, likes } = comment || {};
+  const { user, content, createdAt } = comment || {};
 
   return (
     <>
@@ -20,6 +21,12 @@ const Comment = ({ comment }) => {
         <div className="flex-1">
           <span className="font-medium text-xl mr-4">{user?.userName}</span>
           <CommentBody content={content} />
+
+          <p className="text-lg text-gray-500">
+            {`${moment(new Date(createdAt), "YYYYMMDD").fromNow()}, ${moment(
+              new Date(createdAt)
+            ).format("LL")}`}
+          </p>
         </div>
         <CommentFooter comment={comment} />
       </div>
--- src/components/Comments/Comment/CommentFooter.jsx
+++ src/components/Comments/Comment/CommentFooter.jsx
@@ -1,24 +1,35 @@
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { MdModeEditOutline, MdDelete, MdQuickreply } from "react-icons/md";
-import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 
 import useClickOutside from "../../../hooks/useClickOutside";
-import { useDeleteCommentMutation } from "../../../redux/services/commentApi";
+import {
+  useDeleteCommentMutation,
+  useLikeCommentMutation,
+  useDislikeCommentMutation,
+} from "../../../redux/services/commentApi";
+
 import { FullLoader } from "../../";
 import {
   toggleCommentModal,
   updateEditCommentData,
 } from "../../../redux/slices/commentModalSlice";
+import { selectAuth } from "../../../redux/slices/authSlice";
 
 const CommentFooter = ({ comment }) => {
-  const { _id, content, postId } = comment || {};
+  const { _id, content, postId, likes } = comment || {};
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const domNode = useClickOutside(() => setOpen(false));
+  const {
+    user: { _id: loggedInUserId },
+  } = useSelector(selectAuth);
+
+  const isLiked = likes?.find((like) => like === loggedInUserId);
 
   const [
     deleteComment,
@@ -30,6 +41,27 @@ const CommentFooter = ({ comment }) => {
     },
   ] = useDeleteCommentMutation();
 
+  const [
+    likeComment,
+    {
+      isLoading: isLikeCommentLoading,
+      isSuccess: isCommentLikeSuccess,
+      isError: isCommentLikeError,
+      error: commentLikeError,
+    },
+  ] = useLikeCommentMutation();
+
+  const [
+    dislikeComment,
+    {
+      isLoading: isDislikeCommentLoading,
+      isSuccess: isCommentDislikeSuccess,
+      isError: isCommentDislikeError,
+      error: commentDislikeError,
+    },
+  ] = useDislikeCommentMutation();
+
+  //for delete
   useEffect(() => {
     if (isCommentDeleteSuccess) {
       toast.success("Comment Deleted!");
@@ -41,6 +73,32 @@ const CommentFooter = ({ comment }) => {
       );
     }
   }, [isCommentDeleteSuccess, isCommentDeleteError, commentDeleteError]);
+
+  //for like
+  useEffect(() => {
+    if (isCommentLikeSuccess) {
+      toast.success("Comment Liked!");
+    }
+
+    if (isCommentLikeError) {
+      toast.error(
+        commentLikeError?.data?.message || "Could not like the comment!"
+      );
+    }
+  }, [isCommentLikeSuccess, isCommentLikeError, commentLikeError]);
+
+  //for dislike
+  useEffect(() => {
+    if (isCommentDislikeError) {
+      toast.success("Comment Disliked!");
+    }
+
+    if (commentDislikeError) {
+      toast.error(
+        commentDislikeError?.data?.message || "Could not dislike the comment!"
+      );
+    }
+  }, [isCommentDislikeError, isCommentDislikeSuccess, commentDislikeError]);
 
   const handleDelete = () => {
     setOpen(false);
@@ -58,9 +116,21 @@ const CommentFooter = ({ comment }) => {
     );
   };
 
+  const handleLike = () => {
+    if (isLiked) {
+      dislikeComment(_id);
+    } else {
+      likeComment(_id);
+    }
+  };
+
   return (
     <>
-      {isDeleteCommentLoading ? <FullLoader /> : null}
+      {isDeleteCommentLoading ||
+      isLikeCommentLoading ||
+      isDislikeCommentLoading ? (
+        <FullLoader />
+      ) : null}
       <div className="menu" ref={domNode}>
         <BiDotsVerticalRounded
           className="text-4xl text-gray-700 cursor-pointer"
@@ -71,9 +141,9 @@ const CommentFooter = ({ comment }) => {
             open ? "active" : ""
           }`}
         >
-          <li className="flex gap-1 items-center cursor-pointer hover:text-blue-600 duration-150">
+          {/* <li className="flex gap-1 items-center cursor-pointer hover:text-blue-600 duration-150">
             <MdQuickreply /> Reply
-          </li>
+          </li> */}
           <li
             className="flex gap-1 items-center cursor-pointer hover:text-blue-600 duration-150"
             onClick={handleUpdate}
@@ -91,7 +161,20 @@ const CommentFooter = ({ comment }) => {
         </ul>
       </div>
 
-      <AiOutlineHeart className="ml-auto text-4xl flex-shrink-0" />
+      <div>
+        {isLiked ? (
+          <AiFillHeart
+            className="ml-auto text-4xl flex-shrink-0 text-red-500"
+            onClick={handleLike}
+          />
+        ) : (
+          <AiOutlineHeart
+            className="ml-auto text-4xl flex-shrink-0"
+            onClick={handleLike}
+          />
+        )}
+        <span>{likes?.length || 0} likes</span>
+      </div>
     </>
   );
 };
